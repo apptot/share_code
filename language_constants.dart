89ea331b13873bddc631313349d8ea4181934fd2
samory2016 <72741253+samory2016@users.noreply.github.com>
2023-10-21 20:23:14 +0700

    21.10

---
+++ language_constants.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:flutterubertut/localization/demo_localization.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+const String LAGUAGE_CODE = 'languageCode';
+
+//languages code
+const String ENGLISH = 'en';
+const String FARSI = 'fa';
+const String ARABIC = 'ar';
+const String HINDI = 'hi';
+const String VIETNAMESE = 'vi';
+
+Future<Locale> setLocale(String languageCode) async {
+  SharedPreferences _prefs = await SharedPreferences.getInstance();
+  await _prefs.setString(LAGUAGE_CODE, languageCode);
+  return _locale(languageCode);
+}
+
+Future<Locale> getLocale() async {
+  SharedPreferences _prefs = await SharedPreferences.getInstance();
+  String languageCode = _prefs.getString(LAGUAGE_CODE) ?? "en";
+  return _locale(languageCode);
+}
+
+Locale _locale(String languageCode) {
+  switch (languageCode) {
+    case ENGLISH:
+      return Locale(ENGLISH, 'US');
+    case VIETNAMESE:
+      return Locale(VIETNAMESE, 'VN');
+    case FARSI:
+      return Locale(FARSI, "IR");
+    case ARABIC:
+      return Locale(ARABIC, "SA");
+    case HINDI:
+      return Locale(HINDI, "IN");
+    default:
+      return Locale(ENGLISH, 'US');
+  }
+}
+
+String? getTranslated(BuildContext context, String key) {
+  return DemoLocalization.of(context)!.translate(key);
+}
